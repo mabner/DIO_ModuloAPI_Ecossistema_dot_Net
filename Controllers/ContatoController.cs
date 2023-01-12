5dc361f93441eb20c72fb3122f5988b6dfd11bd5
Marcos Leite <m.abner@live.co.uk>
2023-01-12 12:47:31 -0300

    implements the delete method

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -57,5 +57,19 @@ namespace ModuloAPI.Controllers
 
 			return Ok(contatoBanco);
 		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Deletar(int id)
+		{
+			var contatoBanco = _context.Contatos.Find(id);
+
+			if (contatoBanco == null)
+				return NotFound();
+
+			_context.Contatos.Remove(contatoBanco);
+			_context.SaveChanges();
+
+			return NoContent();
+		}
 	}
 }
